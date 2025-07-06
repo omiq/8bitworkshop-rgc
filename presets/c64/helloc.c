7075f2d0520966c2ade1a7371c12cf3b956eacc7
Chris Garrett <chris@chrisg.com>
2025-07-06 19:21:44 +0100

    my changes

--- presets/c64/helloc.c
+++ presets/c64/helloc.c
@@ -6,11 +6,27 @@
 
 void main(void) {
   clrscr();			// clear screen
-  puts("Hello World!\n");	// write message at cursor
-  chline(12);			// horizontal line
+  puts(" Welcome Adventurer!\n");// write message at cursor
+  chline(22);			// horizontal line
   bordercolor(COLOR_LIGHTBLUE);	// set color to blue
-  bgcolor(COLOR_GREEN);		// set background color
+  bgcolor(COLOR_BLUE);		// set background color
   textcolor(COLOR_YELLOW);	// set text color
-  puts("\nThis text is yellow!\n"); // write message
-  cgetc();			// wait for input
+  
+  // write message
+  puts("\n\n This IDE and emulation tool\n"); 
+  puts(" Is forked from Steven E. Hugg\n"); 
+  puts(" Under the GPL-3.0 license\n");
+
+  
+  textcolor(COLOR_WHITE);	// set text color
+  puts("\n\n - Chris Garrett\n   @RetroGameCoders");
+  
+  textcolor(COLOR_LIGHTBLUE);	// set text color  
+  puts("\n\n Press a Key to Continue\n"); 
+  
+  // wait for input
+  cgetc();
+  // clear the screen
+  clrscr();
 }
+  
